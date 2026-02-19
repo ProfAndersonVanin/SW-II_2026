1729b2dee7e131ff0113e978dec462c648b1fc74
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2026-02-19 10:46:29 -0300

    Códigos de exemplo

---
+++ exemplos/aula01/exemplo03.php
@@ -0,0 +1,17 @@
+<?php
+    $nota = 7;
+    if ($nota >= 6) {
+        echo "Aprovado!";
+    } else {
+        echo "Reprovado!";
+    }
+    echo "<br>";
+    for ($i = 1; $i <= 5; $i++) {
+        if ($i % 2 == 1){
+            $texto = "Aprovado";
+        }else{
+            $texto = "Reprovado";
+        }
+        echo "Número: " . $i . " ==> ". $texto . "<br>";
+    }
+?> 
